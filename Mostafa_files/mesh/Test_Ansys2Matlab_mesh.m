542d35f09c3ab12e2e42976eabb7d079cbc29767
mostafaba8dadi <mostafabaghdadi1995@gmail.com>
2021-03-27 00:00:00 +0200

    Mostafa_files\

---
+++ Mostafa_files/mesh/Test_Ansys2Matlab_mesh.m
@@ -0,0 +1,10 @@
+clear all
+close all
+meshfile='MeshFreqDividerFull_QUAD4.dat';
+meshtype='QUAD4';
+
+model=Ansys2Matlab_mesh(meshfile,meshtype);
+
+PlotMesh(model.nodes(:,2:3),model.elements(:,2:end),0)
+
+
