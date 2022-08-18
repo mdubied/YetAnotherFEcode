96571077edf64a716ef33fe9956749e3e55f323f
mdubied <mdubied@ethz.ch>
2022-08-18 09:17:37 +0200

    Implement getSkin2D for QUAD4

---
+++ examples/Mechanical/2D/Beam2D_Hydrodynamic.m
@@ -0,0 +1,79 @@
+% ------------------------------------------------------------------------ 
+% Modification of Beam2D to include velocity dependent forces
+% 
+% Last modified: 18/08/2022, Mathieu Dubied, ETH Zürich
+%
+% ------------------------------------------------------------------------
+clear; 
+close all; 
+clc
+
+whichModel = 'CUSTOM'; % or "ABAQUS"
+elementType = 'QUAD4';
+% elementType = 'QUAD8';
+
+%% PREPARE MODEL                                                    
+
+% DATA ____________________________________________________________________
+E       = 70e9;     % Young's modulus [Pa]
+rho     = 2700;     % density [kg/m^3]
+nu      = 0.33;     % Poisson's ratio 
+thickness = .1;     % [m] beam's out-of-plane thickness
+
+% Material
+myMaterial = KirchoffMaterial();
+set(myMaterial,'YOUNGS_MODULUS',E,'DENSITY',rho,'POISSONS_RATIO',nu);
+myMaterial.PLANE_STRESS = true;	% set "false" for plane_strain
+% Element
+switch elementType
+    case 'QUAD4'
+        myElementConstructor = @()Quad4Element(thickness, myMaterial);
+    case 'QUAD8'
+        myElementConstructor = @()Quad8Element(thickness, myMaterial);
+end
+
+% MESH_____________________________________________________________________
+Lx = 3;
+Ly = .2;
+nx = 5;
+ny = 3;
+switch upper( whichModel )
+    case 'CUSTOM'
+        [nodes, elements, nset] = mesh_2Drectangle(Lx,Ly,nx,ny,elementType);
+    case 'ABAQUS'
+        % Alternatively, one can write an input file in ABAQUS and read it as:
+        filename = 'Job-BeamQuad';
+        [nodes, elements, nset, elset] = mesh_ABAQUSread(filename);
+end
+
+myMesh = Mesh(nodes);
+myMesh.create_elements_table(elements,myElementConstructor);
+
+% MESH > BOUNDARY CONDITONS
+switch upper( whichModel )
+    case 'CUSTOM'
+        myMesh.set_essential_boundary_condition([nset{1} nset{3}],1:2,0)
+    case 'ABAQUS'
+        myMesh.set_essential_boundary_condition([nset{1} nset{2}],1:2,0)
+end
+
+% ASSEMBLY ________________________________________________________________
+BeamAssembly = Assembly(myMesh);
+M = BeamAssembly.mass_matrix();
+nNodes = size(nodes,1);
+u0 = zeros( myMesh.nDOFs, 1);
+[K,~] = BeamAssembly.tangent_stiffness_and_force(u0);
+
+% store matrices
+BeamAssembly.DATA.K = K;
+BeamAssembly.DATA.M = M;
+%f = QuadAssembly.vector('drag_force', u, ud);
+
+
+%% PLOT MESH WITH NODES AND ELEMENTS
+elementPlot = elements(:,1:4); % plot only corners (otherwise it's a mess)
+figure('units','normalized','position',[.2 .1 .6 .8])
+PlotMesh(nodes, elementPlot, 1);
+
+%% GET OUTER SURFACE
+[skin,allfaces] = getSkin2D(elements);
---
+++ src/PostProcessing/getSkin2D.m
@@ -0,0 +1,52 @@
+% -------------------------------------------------------------------------
+% getSkin2D(elements): computes the external faces of the mesh. 
+% INPUTS:
+%   elements: table of elements (Nelements X Ndofs)
+% OUTPUTS:
+%   skin: table of nodes labels of external faces. Each column contains the
+%         nodes of one face.
+%   allfaces: as skin, but with all the faces.
+%
+% Supported elements: QUAD4
+% Note: a large portion of the code is taken from getSkin3D
+% Last modified: 18/08/2022, Mathieu Dubied, ETH Zürich
+% -------------------------------------------------------------------------
+function [skin,allfaces] = getSkin2D(elements)
+
+nnel  = size(elements,2); % number of nodes per element
+
+% select faces order
+switch nnel
+    case 4 % QUAD4
+        faces = [1 2; 2 3; 3 4; 4 1];
+end
+
+% build face matrix
+N = size(elements,1);
+FACES = zeros(size(faces,2),N*size(faces,1));
+count = 1;
+for ii = 1:N
+    vertici = elements(ii,:);
+    for jj = 1:size(faces,1)
+        FACES(:,count) = vertici(faces(jj,:))';
+        count = count+1;
+    end
+end
+
+% function [x1,icc] = OnlyNotRepeatedColumns(x)____________________________
+x = FACES;
+xs = sort(x,1);
+[~,ia,ic] = unique(xs','rows','stable');
+ia = ia';
+ic = ic';
+ic = ia(ic); % a unique index is given to each unique column
+icc = hist(ic,1:length(ic));	% counts the occurences 
+                                % (occ. after the first appear as zeros)
+icc = icc == 1;                 % take only terms occurring ONCE
+% x1 = xs(:,icc);__________________________________________________________
+
+% remove all the repeated faces (internal), so to plot only external ones
+skin = FACES(:,icc);
+allfaces = FACES;
+
+
