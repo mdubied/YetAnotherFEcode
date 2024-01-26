347242689966119502be6b90decee23a0ec2ff47
Mathieu Dubied <mdubied@ethz.ch>
2024-01-26 20:36:05 +0100

    AO, Fourier series
    converged. Actually tries to maximise the amplitude rather than to create a step function.

--- external/NumericalExperiments/actuation_optimisation.m
+++ external/NumericalExperiments/actuation_optimisation.m
@@ -1,7 +1,7 @@
 % ------------------------------------------------------------------------ 
-% 3D actuation signal optimisation of a fish.
+% Actuation signal optimisation of a fish.
 % 
-% Last modified: 03/12/2023, Mathieu Dubied, ETH Zurich
+% Last modified: 26/01/2024, Mathieu Dubied, ETH Zurich
 %
 % ------------------------------------------------------------------------
 clear; 
@@ -12,7 +12,6 @@ elementType = 'TET4';
 
 FORMULATION = 'N1t'; % N1/N1t/N0
 VOLUME = 1;         % integration over defected (1) or nominal volume (0)
-
 USEJULIA = 0;
 
 %% PREPARE MODEL                                                    
@@ -32,14 +31,11 @@ switch elementType
     case 'TET4'
         myElementConstructor = @()Tet4Element(myMaterial);
 end
-% MESH ____________________________________________________________________
 
+% MESH ____________________________________________________________________
 % nominal mesh
 filename = '3d_fish_for_mike';%'3d_rectangle_660el';%'fish3_664el';
 [nodes, elements, ~, elset] = mesh_ABAQUSread(filename);
-
-% nodes = nodes*0.01;
-% nodes(:,2)=0.8*nodes(:,2);
 
 MeshNominal = Mesh(nodes);
 MeshNominal.create_elements_table(elements,myElementConstructor);
@@ -64,14 +60,6 @@ for el=1:nel
         end
     end   
 end
-% for l=1:length(nset)
-%     MeshNominal.set_essential_boundary_condition([nset{l}],2,0)   %
-%     fixed head
-%     MeshNominal.set_essential_boundary_condition([nset{l}],2,0)     % head on "rail"
-% end
-
-
-
 
 %% OPTIMIZATION PARAMETERS
 dSwim = [1;0;0]; %swimming direction
@@ -84,31 +72,8 @@ tmax = 2.0;
 % The initial conditions should be changed in the following script:
 %   optimise_actuation_3D
 
-% AO 1
-A = [1 0 0 0;
-    -1 0 0 0;
-    0 1 0 0;
-    0 -1 0 0;
-    0 0 1 0;
-    0 0 -1 0;
-    0 0 0 1;
-    0 0 0 -1];
-b = [1.15;-0.75;0.4;0.4;0.2;0.2;1.3;-0.7];
-
-tStart = tic;
-[xiStar,xiEvo,LEvo, ~] = optimise_actuation_3D(myElementConstructor,nset, ...
-    nodes,elements,dSwim,h,tmax,A,b, ...
-    'maxIteration',30, ...
-    'convCrit',0.0005, ...
-    'convCritCost',0.002, ...
-    'barrierParam',600, ...
-    'gStepSize',0.05, ...
-    'nResolve',8, ...
-    'resolveThreshold',0.1);
-topti = toc(tStart);
-fprintf('Computation time: %.2fmin\n',topti/60)
-
 %% AO1
+% actuation_force_6, ran with k=300
 AActu = [1 0 0;
         -1 0 0;
         0 1 0;
@@ -120,7 +85,7 @@ barrierParam = [3,3,30,30,30,30];
 gradientWeights = [0.4,10,10];
 
 tStart = tic;
-[xiStar,xiEvo,LEvo, LwoBEvo] = optimise_actuation_3D(myElementConstructor,nset, ...
+[pStar,pEvo,LEvo, LwoBEvo] = optimise_actuation_3D(myElementConstructor,nset, ...
     nodes,elements,dSwim,h,tmax,AActu,bActu, ...
     'maxIteration',30, ...
     'convCrit',0.02, ...
@@ -133,28 +98,34 @@ tStart = tic;
 topti = toc(tStart);
 fprintf('Computation time: %.2fmin\n',topti/60)
 
-%%
-% AO 2 (Change the acutation functions if you change the time step h)
-% nParam = int32(tmax/h*0.1+2);
-% A = zeros(2 * nParam, nParam);
-% for i = 1:nParam
-%     A(2*i-1:2*i,i) =[1;-1];
-% end
-% b = 1.3*ones(nParam*2,1);
-% 
-% tStart = tic;
-% [xiStar,xiEvo,LEvo, ~] = optimise_actuation_3D(myElementConstructor,nset, ...
-%     nodes,elements,dSwim,h,tmax,A,b, ...
-%     'maxIteration',30, ...
-%     'convCrit',0.0005, ...
-%     'convCritCost',0.01, ...
-%     'barrierParam',10, ...
-%     'gStepSize',0.04, ...
-%     'nResolve',8, ...
-%     'resolveThreshold',0.1);
-% topti = toc(tStart);
-% fprintf('Computation time: %.2fmin\n',topti/60)
+%% AO 2
+% actuation_force_4, ran with k=300
+A = [1 0 0 0;
+    -1 0 0 0;
+    0 1 0 0;
+    0 -1 0 0;
+    0 0 1 0;
+    0 0 -1 0;
+    0 0 0 1;
+    0 0 0 -1];
+b = [1.15;-0.75;0.4;0.4;0.2;0.2;1.3;-0.7];
 
+barrierParam = [3,3,3,3,3,3,30,30];
+gradientWeights = [2,2,2,0.5];
+
+tStart = tic;
+[pStar,pEvo,LEvo,LwoBEvo] = optimise_actuation_3D(myElementConstructor,nset, ...
+    nodes,elements,dSwim,h,tmax,A,b, ...
+    'maxIteration',50, ...
+    'convCrit',0.002, ...
+    'convCritCost',0.002, ...
+    'barrierParam',barrierParam, ...
+    'gradientWeights',gradientWeights, ...
+    'gStepSize',0.003, ...
+    'nResolve',12, ...
+    'resolveThreshold',0.25);
+topti = toc(tStart);
+fprintf('Computation time: %.2fmin\n',topti/60)
 
 %% PLOT COST FUNCTION OVER ITERATIONS _____________________________________
 
@@ -162,8 +133,6 @@ fCost = figure;
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 set(groot,'defaultAxesTickLabelInterpreter','latex'); 
-% plot(LEvo)
-% hold on
 plot(LwoBEvo)
 grid on
 ylabel('$$L$$','Interpreter','latex')
@@ -178,13 +147,14 @@ set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 set(groot,'defaultAxesTickLabelInterpreter','latex'); 
 timePlot = linspace(0,tmax-h,tmax/h);
-p0 = [0.2;2;0];
+%p0 = [0.2;2;0];
+p0 = [1;0;0;1];
 k=300;
 actu0 = zeros(1,length(timePlot));
 actuStar = zeros(1,length(timePlot));
 for i = 1:length(timePlot)
-    actu0(i) = actuation_signal_6(k,timePlot(i),p0);
-    actuStar(i) = actuation_signal_6(k,timePlot(i),xiStar);
+    actu0(i) = actuation_signal_4(k,timePlot(i),p0);
+    actuStar(i) = actuation_signal_4(k,timePlot(i),pStar);
 end
 plot(timePlot,actu0,'--')
 hold on
@@ -202,17 +172,17 @@ set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 set(groot,'defaultAxesTickLabelInterpreter','latex'); 
 subplot(1,3,1)
-plot(xiEvo(1,:));
+plot(pEvo(1,:));
 ylabel('$$p_1$$','Interpreter','latex')
 xlabel('Iterations')
 grid on
 subplot(1,3,2)
-plot(xiEvo(2,:));
+plot(pEvo(2,:));
 ylabel('$$p_2$$','Interpreter','latex')
 xlabel('Iterations')
 grid on
 subplot(1,3,3)
-plot(xiEvo(3,:));
+plot(pEvo(3,:));
 ylabel('$$p_3$$','Interpreter','latex')
 xlabel('Iterations')
 grid on
